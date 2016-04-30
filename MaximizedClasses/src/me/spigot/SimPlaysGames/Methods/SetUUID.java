89177ff45b45f9362eaf44e0a15c5b807b29b079
DevOG <minecraftprogamers101@gmail.com>
2016-04-30 15:27:07 -0400

    UUID system.
    This should help you with wanted to have an extra change while being a VIP

--- MaximizedClasses/src/me/spigot/SimPlaysGames/Methods/SetUUID.java
+++ MaximizedClasses/src/me/spigot/SimPlaysGames/Methods/SetUUID.java
@@ -12,16 +12,21 @@ import me.spigot.SimPlaysGames.Register.UserDataConfig;
 public class SetUUID {
 	
     public static void setUUID(Player player, String group) {
+    	int i = 0; //this int if for the VIP people, who get to choose their class once more
+    	//Adds one players UUID to Set of Members
+	Set<UUID> uuids = new HashSet<>();
         UserDataConfig userData = new UserDataConfig(MaximizedClasses.getInstance(), "UserData.yml"); // get the config test.yml, if it isn't exists it will be created
-        FileConfiguration userDataConfig = userData.getConfig(); // get its config instance
-       
-        //Adds one players UUID to list Members
-        List<String> list = userDataConfig.contains(group) ? userDataConfig.getStringList(group) : new ArrayList<>();
-        String playerUUID = player.getUniqueId().toString(); //Makes the UUID a string
-       
-        list.add(playerUUID);
-        userDataConfig.set(group, list);
-        userData.saveConfig(); // save it
+        FileConfiguration userDataConfig = userData.getConfig(); // get its config instance.
+        String playerUUID = player.getUniqueId().toString(); //Makes the UUID a string        
+    		if (group.equals("VIP")) { //checks if the person is VIP
+    			if (i == 0) { //if the player has not choosen a kit AND is a VIP
+    			    	i++; //changes the int to 1, because they have equipted the class and are VIP
+    				userDataConfig.set(group, list);
+        			userData.saveConfig(); // save it	
+    			} else { //if the player has already choosen a kit, add them to the uuid so they cant get another one.
+        			uuids.add(player.getUniqueId()); 
+    			}
+    		}
     }
 	
 }
